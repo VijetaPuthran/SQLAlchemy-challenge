0666657ec87992334b1af3ce03c91b61bd9f064a
VP <vijeta.puthran@gmail.com>
2021-03-15 23:41:10 -0500

    pynb and py files updated

--- app.py
+++ app.py
@@ -60,13 +60,7 @@ def home():
             f'<a href="/api/v1.0/<start>/<end>">/api/v1.0/<start>/<end></a> : A list of the minimum temperature, the average temperature, and the maximum temperature for a given start-end date range.<br/>'
             f"<br/>"
             f"------------------------------------------------<br/>")
-   #           f"~~~ datesearch (yyyy-mm-dd)<br/>"
-   #         f"/api/v1.0/datesearch/2015-05-30  ~~~~~~~~~~~ low, high, and average temp for date given and each date after<br/>"
-   #         f"/api/v1.0/datesearch/2015-05-30/2016-01-30 ~~ low, high, and average temp for date given and each date up to and including end date<br/>"
-   #         f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
-   #         f"~ data available from 2010-01-01 to 2017-08-23 ~<br/>"
-   #         f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
+ 
 @app.route("/api/v1.0/precipitaton")
 def precipitation():
     
@@ -104,27 +98,28 @@ def temperature():
 
     return jsonify(temp_data)
 
+# @app.route('/api/v1.0/<start>')
+# def start_date(start):
+#     select = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+#     results =  session.query(*select).\
+#                filter(func.strftime("%Y-%m-%d", Measurement.date) >= start).\
+#                group_by(Measurement.date).\
+#                all()
+
+#     dates = []                       
+#     for result in results:
+#         date_dict = {}
+#         date_dict["Date"] = result[0]
+#         date_dict["Low Temp"] = result[1]
+#         date_dict["Avg Temp"] = result[2]
+#         date_dict["High Temp"] = result[3]
+#         dates.append(date_dict)
+#     return jsonify(dates)
+
 @app.route('/api/v1.0/<start>')
-def start_date(start):
-    select = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-
-    results =  session.query(*select).\
-               filter(func.strftime("%Y-%m-%d", Measurement.date) >= start).\
-               group_by(Measurement.date).\
-               all()
-
-    dates = []                       
-    for result in results:
-        date_dict = {}
-        date_dict["Date"] = result[0]
-        date_dict["Low Temp"] = result[1]
-        date_dict["Avg Temp"] = result[2]
-        date_dict["High Temp"] = result[3]
-        dates.append(date_dict)
-    return jsonify(dates)
-
 @app.route('/api/v1.0/<start>/<end>')
-def startEnd(start, end):
+def startEnd(start, end = '2017-08-23'):
     select = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
     results =  session.query(*select).\
